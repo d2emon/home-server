9817d9e147f055d31268619b51752b73c2fec44e
Dmitry Kutsenko <d2emonium@gmail.com>
2017-05-14 02:37:12 +0300

    audio

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.redirect('/index.html');
+});
+
+router.get('/index.:format?', function(req, res) {
   res.render('index', { 
-    title: 'Express',
+    title: 'About',
     news: [
       {
         time: "2011-11-09",
@@ -73,6 +77,190 @@ router.get('/', function(req, res) {
       text: "Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas aspernatur."
     },
     stores: "/images/page1-img1.jpg"
+  });
+});
+
+router.get('/audio.:format?', function(req, res) {
+  res.render('audio', {
+    title: 'Audio',
+    albums: [
+      {
+        title: "Genres Remixes",
+	image: "/images/albums/page2-img1.jpg",
+	year: "2011",
+	description: "Lorem ipsum dolor consctetur adipisicing elitdo eusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+	tracks: [
+	  {
+            id: "01",
+	    title: "Duis aute irure dolor"
+	  },
+	  {
+            id: "02",
+	    title: "In reprehenderit in voluptate"
+	  },
+	  {
+            id: "03",
+	    title: "Velit esse cillum dolore"
+	  },
+	  {
+            id: "04",
+	    title: "Eu fugiat nulla pariatur"
+	  },
+	  {
+            id: "05",
+	    title: "Excepteur sint occaecat"
+	  },
+	  {
+            id: "06",
+	    title: "Cupidatat non"
+	  },
+	  {
+            id: "07",
+	    title: "Proident sunt in"
+	  },
+	  {
+            id: "08",
+	    title: "Culpa qui officia deserunt mollit"
+	  },
+	  {
+            id: "09",
+	    title: "Anim id est laborum"
+	  },
+	  {
+            id: "10",
+	    title: "At vero eos et accusamus"
+	  },
+	  {
+            id: "11",
+	    title: "Et iusto odio dignissimos"
+	  }
+	]
+      },
+      {
+        title: "Genres",
+	image: "/images/albums/page2-img2.jpg",
+	year: "2010",
+	description: "Lorem ipsum dolor consctetur adipisicing elitdo eusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+	tracks: [
+	  {
+            id: "01",
+	    title: "Duis aute irure dolor"
+	  },
+	  {
+            id: "02",
+	    title: "In reprehenderit in voluptate"
+	  },
+	  {
+            id: "03",
+	    title: "Velit esse cillum dolore"
+	  },
+	  {
+            id: "04",
+	    title: "Eu fugiat nulla pariatur"
+	  },
+	  {
+            id: "05",
+	    title: "Excepteur sint occaecat"
+	  },
+	  {
+            id: "06",
+	    title: "Cupidatat non"
+	  },
+	  {
+            id: "07",
+	    title: "Proident sunt in"
+	  },
+	  {
+            id: "08",
+	    title: "Culpa qui officia deserunt mollit"
+	  },
+	  {
+            id: "09",
+	    title: "Anim id est laborum"
+	  },
+	  {
+            id: "10",
+	    title: "At vero eos et accusamus"
+	  }
+	]
+      },
+      {
+        title: "Rock & Roll",
+	image: "/images/albums/page2-img3.jpg",
+	year: "2010",
+	description: "Lorem ipsum dolor consctetur adipisicing elitdo eusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+	tracks: [
+	  {
+            id: "01",
+	    title: "Duis aute irure dolor"
+	  },
+	  {
+            id: "02",
+	    title: "In reprehenderit in voluptate"
+	  },
+	  {
+            id: "03",
+	    title: "Velit esse cillum dolore"
+	  },
+	  {
+            id: "04",
+	    title: "Eu fugiat nulla pariatur"
+	  },
+	  {
+            id: "05",
+	    title: "Excepteur sint occaecat"
+	  },
+	  {
+            id: "06",
+	    title: "Cupidatat non"
+	  },
+	  {
+            id: "07",
+	    title: "Proident sunt in"
+	  },
+	  {
+            id: "08",
+	    title: "Culpa qui officia deserunt mollit"
+	  },
+	  {
+            id: "09",
+	    title: "Anim id est laborum"
+	  },
+	  {
+            id: "10",
+	    title: "At vero eos et accusamus"
+	  },
+	  {
+            id: "11",
+	    title: "Et iusto odio dignissimos"
+	  }
+	]
+      }
+    ]
+  });
+});
+
+router.get('/video.:format?', function(req, res) {
+  res.render('video', { 
+    title: 'Video'
+  });
+});
+
+router.get('/gallery.:format?', function(req, res) {
+  res.render('gallery', { 
+    title: 'Gallery'
+  });
+});
+
+router.get('/tour-dates.:format?', function(req, res) {
+  res.render('tours', { 
+    title: 'Tour Dates'
+  });
+});
+
+router.get('/contacts.:format?', function(req, res) {
+  res.render('contacts', { 
+    title: 'Contacts'
   });
 });
 
