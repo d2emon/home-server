fa9a3a2d654f5093c5b8925c63bc0f75f17c5168
Dmitry Kutsenko <d2emonium@gmail.com>
2017-05-18 16:22:11 +0300

    Games template

--- src/routes/games.js
+++ src/routes/games.js
@@ -58,6 +58,106 @@ router.get('/index.:format?', function(req, res) {
             "/images/slider/img2.jpg",
             "/images/slider/img3.jpg"
           ],
+          subtitle: "Morbi volutpat leo in ligula. Integer vel magna. Quisque"
+	    + " ut magna et nisi bibendum sagittis. Fusce",
+          items: [
+            "Morbi volutpat leo in",
+            "ligula. Integer vel lorem",
+            "magna. Quisque ut",
+            "umagna et nisi bibendum",
+            "sagittis. Fusce elit ligula,",
+            "sodales sit amet",
+            "Morbi volutpat leo in",
+            "ligula. Integer vel lorem",
+            "magna. Quisque ut",
+            "umagna et nisi bibendum",
+            "sagittis. Fusce elit ligula,",
+            "sodales sit amet"
+          ],
+          reviews: [
+            {
+              image: "/images/games/img_previews.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 67,
+	    },
+            {
+              image: "/images/games/img_previews-12.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 82,
+	    },
+            {
+              image: "/images/games/img_previews-15.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 79,
+	    },
+            {
+              image: "/images/games/img_previews-16.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 91,
+	    },
+          ],
+          newGames: [
+            {
+              image: "/images/games/img_new.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 67,
+	    },
+            {
+              image: "/images/games/img_new-11.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 82,
+	    },
+            {
+              image: "/images/games/img_new-13.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 79,
+	    },
+            {
+              image: "/images/games/img_new-14.jpg",
+              title: "Morbi volutpat leo in ligula.",
+	      text: "<a href=\"#\">Cras quam augue, consectetuer</a> ut, "
+                + "tincidunt a, velit. Quisque euismod tortor sed nulla. Nunc"
+                + " dapibus, nisi et iaculis",
+              score: 91,
+	    },
+          ],
+          random: {
+            title: "",
+            image: "/images/games/img_feat.jpg",
+            text: "Morbi volutpat leo in ligula. Integer vel magna. "
+              + "<a href=\"#\">Quisque ut magna</a> et nisi bibendum "
+              + "sagittis. Fusce",
+            score: 76,
+          },
+          wanted: {
+            title: "",
+            image: "/images/games/img_wanted.jpg",
+            text: "Morbi volutpat leo in ligula. Integer vel magna. "
+              + "<a href=\"#\">Quisque ut magna</a> et nisi bibendum "
+              + "sagittis. Fusce",
+            score: 76,
+          },
           news: news, 
 	  artist: artist,
           events: events,
