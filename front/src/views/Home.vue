e25776a65a556a46167beef4e2a2267f7f466283
Dmitry Kutsenko <d2emonium@gmail.com>
2019-10-09 22:09:18 +0300

    Update assets

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,30 +1,14 @@
 <template>
-    <v-app>
-        <v-toolbar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Vuetify</span>
-                <span class="font-weight-light">MATERIAL DESIGN</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn
-                    flat
-                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                    target="_blank"
-            >
-                <span class="mr-2">Latest Release</span>
-            </v-btn>
-        </v-toolbar>
+  <HelloWorld />
+</template>
 
-        <v-content>
-            Home:
-            <HelloWorld />
-        </v-content>
-    </v-app>
-  </template>
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default Vue.extend({
   components: {
-    HelloWorld: () => import('@/components/HelloWorld.vue'),
+    HelloWorld,
   },
-},
+});
 </script>
