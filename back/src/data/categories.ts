532f8cbee071559bb4aa0717b80e475e661c87d9
Dmitry Kutsenko <d2emonium@gmail.com>
2020-06-15 18:13:33 +0300

    Get categories from file

---
+++ back/src/data/categories.ts
@@ -0,0 +1,123 @@
+interface Category {
+  itemId: number;
+  slug: string;
+  title: string;
+  image?: string;
+  text?: string;
+}
+
+export default [
+  {
+    "itemId": 1,
+    "slug": "games",
+    "url": "/categories/games",
+    "title": "Игры",
+    "text": "Что наша жизнь - игра&hellip;"
+  },
+  {
+    "itemId": 2,
+    "slug": "art",
+    "url": "/categories/art",
+    "title": "Искусство",
+    "text": "Статьи об искусстве и творчестве"
+  },
+  {
+    "itemId": 3,
+    "slug": "history",
+    "url": "/categories/history",
+    "title": "История",
+    "text": "В детстве у меня была таблица для сравнения разных исторических событий - какие из них произошли раньше, а какие позже. К тому же этот проект поможет мне пополнить отсутствие знаний по истории."
+  },
+  {
+    "itemId": 4,
+    "slug": "culture",
+    "url": "/categories/culture",
+    "title": "Культура",
+  },
+  {
+    "itemId": 5,
+    "slug": "lugansk",
+    "url": "/categories/lugansk",
+    "title": "Луганск",
+    "text": "Луганск - моя родина и я, черт возьми, люблю этот город"
+  },
+  {
+    "itemId": 6,
+    "slug": "management",
+    "url": "/categories/management",
+    "title": "Менеджмент"
+  },
+  {
+    "itemId": 7,
+    "slug": "worlds",
+    "url": "/categories/worlds",
+    "title": "Миры",
+    "text": "Эти миры, созданные исключительно фантазией их авторов, получились настолько красивыми и интересными, что многим бы хотелось, чтобы они существовали в реальности. К сожалению, экскурсия по ним возможна только виртуальная."
+  },
+  {
+    "itemId": 8,
+    "slug": "science",
+    "url": "/categories/science",
+    "title": "Наука",
+    "text": "Материалы по точным наукам"
+  },
+  {
+    "itemId": 9,
+    "slug": "parascience",
+    "url": "/categories/parascience",
+    "title": "Паранаучное",
+    "text": "Всякие ненаучные, псевдонаучные и паранаучные исследования"
+  },
+  {
+    "itemId": 10,
+    "slug": "nature",
+    "url": "/categories/nature",
+    "title": "Природа",
+    "text": "Этот проект является отражением моих увлечений биологией. Все, что касается растений, животных и всего живого на земле отправится в этот проект"
+  },
+  {
+    "itemId": 11,
+    "slug": "psychology",
+    "url": "/categories/psychology",
+    "title": "Психология"
+  },
+  {
+    "itemId": 12,
+    "slug": "work",
+    "url": "/categories/work",
+    "title": "Работа"
+  },
+  {
+    "itemId": 13,
+    "slug": "tech",
+    "url": "/categories/tech",
+    "title": "Техника"
+  },
+  {
+    "itemId": 14,
+    "slug": "quotes",
+    "url": "/categories/quotes",
+    "title": "Цитаты",
+    "text": "Цитаты и афоризмы"
+  },
+  {
+    "itemId": 15,
+    "slug": "economics",
+    "url": "/categories/economics",
+    "title": "Экономика"
+  },
+  {
+    "itemId": 16,
+    "slug": "humor",
+    "url": "/categories/humor",
+    "title": "Юмор",
+    "text": "Разные приколы"
+  },
+  {
+    "itemId": 17,
+    "slug": "lingua",
+    "url": "/categories/lingua",
+    "title": "Языки",
+    "text": "Полезно иногда знать иностранные языки"
+  }
+];
